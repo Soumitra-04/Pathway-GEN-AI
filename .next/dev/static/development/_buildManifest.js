7976f68e35865e057300eaa2dcaac278d4d0586b
= <jshreeya05@gmail.com>
2025-12-06 20:12:44 +0530

    Changed file tree

--- .next/dev/static/development/_buildManifest.js
+++ .next/dev/static/development/_buildManifest.js
@@ -1,12 +1,20 @@
 self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/pages/index.js"
+  ],
+  "/_error": [
+    "static/chunks/pages/_error.js"
+  ],
   "__rewrites": {
     "afterFiles": [],
     "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
+    "/",
     "/_app",
     "/_error",
-    "/api/generateBrand/generateBrand"
+    "/api/generateBrand/route",
+    "/api/regenerateLogo/route"
   ]
 };self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
